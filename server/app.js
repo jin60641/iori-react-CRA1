c5dc2f50ed23d75fcaca306cb77aba996a455555
진상현 <jin60641@naver.com>
2018-03-24 23:25:58 +0900

    excute node with process port

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use('/public',express.static(path.resolve(__dirname,'..','public')));
-let port = 3000;
+let port = process.evn.PORT||3000;
 if( process.env.NODE_ENV == 'api' ){
 	port = 3333;
 } else {
